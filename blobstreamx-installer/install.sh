6b95d4a21eeefad7aa86dfe0a4aa108689000b54
pops <infra@pops.one>
2024-04-07 18:26:44 +0700

    [blobstreamx-installer] non-interactive rust install

--- blobstreamx-installer/install.sh
+++ blobstreamx-installer/install.sh
@@ -104,7 +104,8 @@ docker pull succinctlabs/succinct-local-prover
 
 echo
 echo "Install rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 
 source ~/.profile
 
